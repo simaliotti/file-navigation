c7159cad3332d4d0957594fa1a42494ef7609958
Simon Aliotti <sim.aliotti@gmail.com>
2019-01-15 17:01:16 +0100

    add object stream method

--- src/com/aliottisimon/Main2.java
+++ src/com/aliottisimon/Main2.java
@@ -4,15 +4,21 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.sun.javafx.iio.ios.IosDescriptor;
 
 public class Main2 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-		//copyFile();
-		copyFileWithBuffer();
+		// copyFile();
+		// copyFileWithBuffer();
+		writeReadObjectStream();
 
 	}
 
@@ -102,7 +108,7 @@ public class Main2 {
 	}
 
 	public static void copyFileWithBuffer() throws IOException {
-		
+
 		File fileToRead = new File("C:\\dossier\\monkey.jpg");
 		File file = null;
 
@@ -111,16 +117,12 @@ public class Main2 {
 
 		BufferedInputStream bis = null;
 		BufferedOutputStream bos = null;
-		
+
 		try {
 
-		
-					
 			file = new File("C:\\dossier\\copy.jpg");
 			fop = new FileOutputStream(file);
 
-			
-			
 			if (!file.exists()) {
 				file.createNewFile();
 			}
@@ -129,18 +131,61 @@ public class Main2 {
 			int data;
 			bis = new BufferedInputStream(fis);
 			bos = new BufferedOutputStream(fop);
-			
-			
+
 			while ((data = bis.read()) >= 0) {
 				bos.write(data);
 			}
-			
-			//bos.flush();
+
+			// bos.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			
+
 		}
 	}
-	
-}
+
+	public static void writeReadObjectStream() throws FileNotFoundException, IOException, ClassNotFoundException {
+		// ecrit dans le fichier
+		File file = new File("C:\\dossier\\test3.txt");
+
+		try (FileOutputStream fop = new FileOutputStream(file); ObjectOutputStream oop = new ObjectOutputStream(fop)) {
+			int number = 10;
+			double chiffre = 12.5;
+			String hello = "hello";
+			String bye = "bye";
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			oop.writeObject(number);
+			oop.writeObject(chiffre);
+			oop.writeObject(hello);
+			oop.writeObject(bye);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// read content file
+
+		try (FileInputStream fis = new FileInputStream(file); ObjectInputStream ois = new ObjectInputStream(fis)) {
+			
+			
+			int number = (Integer) ois.readObject();
+			double chiffre = (Double) ois.readObject();
+			String hello = (String) ois.readObject();
+			String bye = (String) ois.readObject();
+
+			
+			System.out.println(number);
+			System.out.println(chiffre);
+			System.out.println(hello);
+			System.out.println(bye);
+
+			
+			}
+
+		}
+	}
+
